9e5554686136954724aa693b05c6a5bee36b6ed9
navint1 <navint2@illinois.edu>
2018-12-09 22:46:28 -0600

    updated

---
+++ app/src/main/java/com/example/navin/yoyo/Instructions.java
@@ -0,0 +1,13 @@
+package com.example.navin.yoyo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Instructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructions);
+    }
+}
--- app/src/main/java/com/example/navin/yoyo/MainActivity.java
+++ app/src/main/java/com/example/navin/yoyo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.navin.yoyo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +8,15 @@ import android.widget.EditText;
 import android.widget.Button;
 
 
+
 public class MainActivity extends AppCompatActivity {
     String url;
     EditText urlinput;
     Button uploadbutton;
+    Button howbutton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         urlinput = (EditText) findViewById(R.id.urlinput);
         uploadbutton = (Button) findViewById(R.id.uploadbutton);
+        howbutton = (Button) findViewById(R.id.howbutton);
+
         uploadbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 url = urlinput.getText().toString();
+                System.out.println(url);
             }
         });
+
+        howbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1 = new Intent(MainActivity.this, Instructions.class);
+                startActivity(int1);
+            }
+        });
+
 
 
     }
